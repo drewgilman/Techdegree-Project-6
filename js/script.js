a5d60138d57edbb3d7cb2b354b557390281c9960
Andrew Gilman <drewgilman@aol.com>
2018-09-06 19:58:55 -0500

    Final Project
    Finalized design and features

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const spans= document.querySelectorAll("span.videoCaptions");
 vid.addEventListener("timeupdate", ()=>{
   for (let i = 0; i < spans.length; i += 1 ) {
     if(parseFloat(spans[i].dataset.start) < vid.currentTime && vid.currentTime < parseFloat(spans[i].dataset.end)){
-      spans[i].style.color = "orange";
+      spans[i].style.color = "darkorange";
     } else {
       spans[i].style.color = "black";
     }
