c2de47d11aaa88cf25bc3db1168d4cc7143f6c3b
Andrew Gilman <drewgilman@aol.com>
2018-09-06 19:54:33 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const vid = document.getElementById("projectVideo");
+const spans= document.querySelectorAll("span.videoCaptions");
+
+
+
+
+vid.addEventListener("timeupdate", ()=>{
+  for (let i = 0; i < spans.length; i += 1 ) {
+    if(parseFloat(spans[i].dataset.start) < vid.currentTime && vid.currentTime < parseFloat(spans[i].dataset.end)){
+      spans[i].style.color = "orange";
+    } else {
+      spans[i].style.color = "black";
+    }
+  }
+})
